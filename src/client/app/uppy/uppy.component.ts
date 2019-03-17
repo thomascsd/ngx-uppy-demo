767b3d98a9f065e33835744b5ccbc8035c8e0ee5
thomascsd <thomascsd808@gmail.com>
2019-03-17 15:41:00 +0800

    加上uppy

--- src/client/app/uppy/uppy.component.ts
+++ src/client/app/uppy/uppy.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as Uppy from '@uppy/core';
+import * as Dashboard from '@uppy/dashboard';
+import * as XHRUpload from '@uppy/xhr-upload';
 
 @Component({
   selector: 'app-uppy',
   templateUrl: './uppy.component.html',
   styleUrls: ['./uppy.component.scss']
 })
-export class UppyComponent implements OnInit {
+export class UppyComponent implements AfterViewInit {
+  constructor() {}
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    const uppy = Uppy();
 
-  ngOnInit() {
+    uppy
+      .use(Dashboard, {
+        inline: true,
+        target: '.uploadContainer'
+      })
+      .use(XHRUpload, {
+        endpoint: '/api/upload',
+        formData: true,
+        fieldName: 'files[]'
+      });
   }
-
 }
