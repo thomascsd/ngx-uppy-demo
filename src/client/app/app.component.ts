558ff3b9fb58cd0d7c81ac72acdce4cbf6c1356b
thomascsd <thomascsd808@gmail.com>
2019-05-31 23:42:44 +0800

    upgrade v8.0

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleChange } from '@angular/material';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
--- src/client/app/material/material.module.ts
+++ src/client/app/material/material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSlideToggleModule } from '@angular/material';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   imports: [CommonModule, MatSlideToggleModule],
