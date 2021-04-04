f3b8401443c8e8bb2210197b13a59ac461129d44
thomascsd <thomascsd808@gmail.com>
2021-04-04 22:47:54 +0800

    v11

--- src/client/app/shared/uppy/uppy.component.spec.ts
+++ src/client/app/shared/uppy/uppy.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UppyComponent } from './uppy.component';
 
@@ -6,7 +6,7 @@ describe('UppyComponent', () => {
   let component: UppyComponent;
   let fixture: ComponentFixture<UppyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UppyComponent ]
     })
