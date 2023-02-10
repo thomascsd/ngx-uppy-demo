bd92c0e95c4621bc2c6806a0f670cd8b39863a3c
Thomas Chang <thomascsd808@gmail.com>
2023-02-10 23:17:47 +0800

    add eslint

--- tshook.js
+++
@@ -1,7 +0,0 @@
-const { join } = require('path');
-const projectPath = join(process.cwd(), '/src/server/tsconfig.json');
-
-console.log(`ProjectPath:${projectPath}`);
-require('ts-node').register({
-  project: projectPath
-});
