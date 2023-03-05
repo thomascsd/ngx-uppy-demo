92e6369ef1c02c8d66fa6233a1d24415d095c0e9
thomascsd <thomascsd808@gmail.com>
2023-03-05 12:46:22 +0800

    adjust model's props

--- src/app/core/models/ImageFile.ts
+++
@@ -1,6 +0,0 @@
-import { BaseModel } from './BaseModel';
-export class ImageFile extends BaseModel {
-  filename: string;
-  size: number;
-  mimetype: string;
-}
--- src/app/core/models/image-datum.ts
+++ src/app/core/models/image-datum.ts
@@ -1,14 +1,6 @@
 export interface ImageDatum {
-  handle: number;
-  url: string;
   filename: string;
   size: number;
   mimetype: string;
-  status: string;
-}
-
-export function createImageDatum(params: Partial<ImageDatum>) {
-  return {
-    url: '',
-  } as ImageDatum;
+  content: string;
 }
